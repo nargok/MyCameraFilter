187f128b8996f27d3a7e7b4893c5e8be12d5849f
nargok <nargok@gmail.com>
2021-08-28 16:42:22 +0900

    install RxSwift & permission to access photo library

--- MyCameraFilter/PhotoCollectionViewController.swift
+++ MyCameraFilter/PhotoCollectionViewController.swift
@@ -7,7 +7,26 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotoCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        populatePhotos()
+    }
+    
+    private func populatePhotos() {
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            
+            if status == .authorized {
+                // access the photos from photo libraries
+            }
+            
+        }
+        
+    }
     
 }
