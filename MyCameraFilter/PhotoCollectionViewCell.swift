eb1e327f93d48922a3ff2ad77325ac2a897ece18
nargok <nargok@gmail.com>
2021-08-30 19:29:21 +0900

    display photo in collection view

---
+++ MyCameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  MyCameraFilter
+//
+//  Created by 後閑諒一 on 2021/08/30.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
--- MyCameraFilter/PhotoCollectionViewController.swift
+++ MyCameraFilter/PhotoCollectionViewController.swift
@@ -19,6 +19,36 @@ class PhotoCollectionViewController: UICollectionViewController {
         populatePhotos()
     }
     
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
+            fatalError("PhotoCollectionViewCell is not found")
+        }
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        
+        print(self.images)
+        
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            
+            DispatchQueue.main.async {
+                cell.photoImageView?.image = image // todo ここでエラーになる
+            }
+            
+        }
+        
+        return cell
+    }
+    
     private func populatePhotos() {
         
         PHPhotoLibrary.requestAuthorization { [ weak self ] status in
@@ -32,7 +62,9 @@ class PhotoCollectionViewController: UICollectionViewController {
                 }
                 
                 self?.images.reverse()
-                print(self?.images)
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
             }
             
         }
