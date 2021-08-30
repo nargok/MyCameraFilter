602b0d4c64528616cbab895ffb71681a78e159d3
nargok <nargok@gmail.com>
2021-08-30 20:20:23 +0900

    PublishSubject & subscribe

--- MyCameraFilter/PhotoCollectionViewController.swift
+++ MyCameraFilter/PhotoCollectionViewController.swift
@@ -8,8 +8,14 @@
 import Foundation
 import UIKit
 import Photos
+import RxSwift
 
 class PhotoCollectionViewController: UICollectionViewController {
+    
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
     
     private var images =  [PHAsset]()
     
@@ -27,6 +33,25 @@ class PhotoCollectionViewController: UICollectionViewController {
         return self.images.count
     }
     
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let selectedAsset = self.images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+            
+            guard let info = info else { return }
+            
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage {
+                
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
@@ -35,9 +60,7 @@ class PhotoCollectionViewController: UICollectionViewController {
         
         let asset = self.images[indexPath.row]
         let manager = PHImageManager.default()
-        
-        print(self.images)
-        
+                
         manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
             
             DispatchQueue.main.async {
--- MyCameraFilter/ViewController.swift
+++ MyCameraFilter/ViewController.swift
@@ -9,13 +9,34 @@ import UIKit
 import RxSwift
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         
+// todo これでうまく行かない理由を後で調べる
+//        guard let navC = segue.destination as? UINavigationController,
+//              let _ = navC.viewControllers.first as? PhotoCollectionViewController else {
+//                        fatalError("Segue destination is not found")
+//
+//                }
+        
+//              let photoCVC = navC.viewControllers.first as? PhotoCollectionViewController else {
+//            fatalError("Segue destination is not found")
+//        }
+
+        let photoCVC = segue.destination as? PhotoCollectionViewController // todo 一時的に直接viewContollerを取得
+        photoCVC?.selectedPhoto.subscribe(onNext: {[weak self] photo in
+            self?.photoImageView.image = photo
+        }).disposed(by: disposeBag)
     }
 
 
